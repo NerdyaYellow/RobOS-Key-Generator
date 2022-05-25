ce5ee51cbe57b615c21cd62f8c7f3a38fc041262
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 12:39:15 +0200

    Add project files.

---
+++ RobOS Key Generator/Generator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Extensions.Standard.Randomization;
+
+namespace Program
+{
+    class Generator
+    {
+        private double Floor2Decimals(double d)
+        {
+            return Math.Floor(d * 100) / 100;
+        }
+
+        public void Generate(int keynumber, bool showunchecked, bool makenoise, bool alt, int amount)
+        {
+            // A copy of the Moon Framework in Roblox RobOS
+
+            long ctime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            StrongRandom srandom = new StrongRandom();
+            Random random = new Random();
+
+            int i = 0;
+
+            while (i < amount)
+            {
+                string final;
+                string befinal;
+                int one;
+                int two;
+                int three;
+                if (alt)
+                {
+                    one = random.Next(10000, 99999);
+                    two = random.Next(10000, 99999);
+                    three = random.Next(10000, 99999);
+                }
+                else
+                {
+                    one = srandom.Next(10000, 99999);
+                    two = srandom.Next(10000, 99999);
+                    three = srandom.Next(10000, 99999);
+                }
+                befinal = $"{one}{two}{three}";
+                final = $"{one}-{two}-{three}";
+
+
+                char[] a = befinal.ToCharArray();
+                if ((int.Parse(a[0].ToString()) + int.Parse(a[1].ToString()) + int.Parse(a[2].ToString()) + int.Parse(a[3].ToString()) + int.Parse(a[4].ToString()) + int.Parse(a[5].ToString()) + int.Parse(a[6].ToString()) + int.Parse(a[7].ToString()) + int.Parse(a[8].ToString()) + int.Parse(a[9].ToString()) + int.Parse(a[10].ToString()) + int.Parse(a[11].ToString()) + int.Parse(a[12].ToString()) + int.Parse(a[13].ToString()) + int.Parse(a[14].ToString())) == keynumber)
+                {
+                    i++;
+                    if (makenoise)
+                        Console.Beep(1000, 100);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(final);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if (showunchecked)
+                    {
+                        if (makenoise)
+                            Console.Beep(500, 100);
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(final);
+                        Console.ResetColor();
+                    }
+                }
+            }
+            long nowtime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            Console.WriteLine($"Finished! Time to generate: {Floor2Decimals((double)(nowtime - ctime) / 1000)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RobOS Key Generator/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int keynumber = 0;
+            bool showunchecked = false;
+            bool makenoise = false;
+            bool mta = false;
+            bool alt = false;
+            int amount = 0;
+
+            Console.WriteLine("Welcome to the RobOS Key Generator! Please type the correct RobOS ativation key number.");
+            string line = Console.ReadLine();
+
+            if (int.Parse(line) != 0)
+                keynumber = int.Parse(line);
+            else
+            {
+                Console.WriteLine("Not a number!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Show unverified keys? Y/N");
+            ConsoleKeyInfo line2 = Console.ReadKey();
+            if (line2.Key == ConsoleKey.Y)
+                showunchecked = true;
+            else if (line2.Key == ConsoleKey.N)
+                showunchecked = false;
+            else
+            {
+                Console.WriteLine("Not an answer!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Create beeps of progress? Y/N");
+            ConsoleKeyInfo line3 = Console.ReadKey();
+            if (line3.Key == ConsoleKey.Y)
+                makenoise = true;
+            else if (line3.Key == ConsoleKey.N)
+                makenoise = false;
+            else
+            {
+                Console.WriteLine("Not an answer!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Use Multi Threading? Y/N");
+            ConsoleKeyInfo line4 = Console.ReadKey();
+            if (line4.Key == ConsoleKey.Y)
+                mta = true;
+            else if (line4.Key == ConsoleKey.N)
+                mta = false;
+            else
+            {
+                Console.WriteLine("Not an answer!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Use alternate generator? Y/N");
+            ConsoleKeyInfo line5 = Console.ReadKey();
+            if (line5.Key == ConsoleKey.Y)
+                alt = true;
+            else if (line5.Key == ConsoleKey.N)
+                alt = false;
+            else
+            {
+                Console.WriteLine("Not an answer!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("How many activation keys to generate?");
+            string line6 = Console.ReadLine();
+            try
+            {
+                int.Parse(line6);
+            }
+            catch
+            {
+                Console.WriteLine("Not a number!");
+                Console.ReadKey();
+                Environment.Exit(0);
+
+            }
+            finally
+            {
+                amount = int.Parse(line6);
+            }
+
+            Console.Clear();
+
+            if (mta)
+            {
+                GeneratorMTA gen = new GeneratorMTA();
+                gen.Generate(keynumber, showunchecked, makenoise, alt, amount);
+            }
+            else
+            {
+                Generator gen = new Generator();
+                gen.Generate(keynumber, showunchecked, makenoise, alt, amount);
+            }
+        }
+    }
+}
